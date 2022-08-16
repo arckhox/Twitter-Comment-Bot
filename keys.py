7596f7717a0aa1e73c0a2e01b2f169bde579266b
Arsalan Khosrojerdi <48126435+arckhox@users.noreply.github.com>
2022-08-16 21:11:18 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,7 +2,7 @@
 # This file is not meant to be used in the main script.
 # Instead, copy this file into keys.py and put your own
 # keys there.
-CONSUMER_KEY = 'IPLTT6V480BNbXX7lfMMEEyIv'
-CONSUMER_SECRET = 'CTYGyShgEkgRyLprqcBWdEy1mLbJK0rf9nztdWVvh11iT28Sp2'
-ACCESS_KEY = '950518637051498496-Mx1QhfLF9yVXm0cCFJ4F0L9y02LXGvu'
-ACCESS_SECRET = 'TljYnJUE2bhMzi5RmhEBkjCEDx33KgSm3XkO1xXaMrUlh'
+CONSUMER_KEY = 'APIKEYS'
+CONSUMER_SECRET = 'APIKEYS'
+ACCESS_KEY = 'APIKEYS'
+ACCESS_SECRET = 'APIKEYS'
